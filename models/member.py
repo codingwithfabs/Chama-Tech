52129d0b8a0a39c6d8dac6805ee64e78c1ad77e0
fabian <fabianmbatha1204@gmail.com>
2026-04-30 03:22:23 +0300

    chore: pushing changes before pulling latest changes

--- models/member.py
+++ models/member.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class ChamaMember(models.Model):
     _name = 'chamatech.member'
@@ -8,3 +9,14 @@ class ChamaMember(models.Model):
     name = fields.Char(string="Member Name", required=True, tracking=True)
     phone = fields.Char(string="Mpesa Number", required=True, tracking=True)
     role_id = fields.Many2one('chamatech.role', string="Role", required=True, tracking=True)
+
+    @api.constrains('role_id')
+    def _check_unique_role_assignment(self):
+        for member in self:
+            #Search for other members with the same role
+            duplicate = self.search([
+                ('role_id', '=', member.role_id.id),
+                ('id', '!=', member.id)
+            ])
+            if duplicate:
+                raise ValidationError(f"The role '{member.role_id.name}' is already assigned to {duplicate[0].name}.")
--- models/role.py
+++ models/role.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class ChamaRole(models.Model):
     _name = 'chamatech.role'
@@ -8,3 +9,13 @@ class ChamaRole(models.Model):
     name = fields.Char(string="Role Name", required=True, tracking=True)
     availability = fields.Selection([('available', 'Available'),
                                      ('filled', 'Filled')], string="Availability", default="available")
+    
+
+    @api.constrains('name')
+    def _check_role_limit(self):
+        #Count existing roles
+        role_count = self.env['chamatech.role'].search_count([])
+
+        # If we are trying to create a record that exceeds 11
+        if role_count > 11:
+            raise ValidationError("Limit Reached: This platform supports a maximum of 11 roles/users including the Admin.")
